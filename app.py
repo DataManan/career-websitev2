fa78230378d8b00ec3a78a52970df2dfe1ad57b6
Manan Jain <101595148+DataManan@users.noreply.github.com>
2023-02-17 14:20:31 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {  
+    "ID": 1,
+    "ROLE": "Data Analyst",
+    "LOCATION": "Bengaluru, India",
+    "PAY": "Rs. 10,00,000"
+  },
+  {  
+    "ID": 2,
+    "ROLE": "Data Scientist",
+    "LOCATION": "Mumbai, India",
+    "PAY": "Rs. 15,00,000"
+  },
+  {  
+    "ID": 3,
+    "ROLE": "Frontend Engineer",
+    "LOCATION": "Bengaluru, India",
+    "PAY": "Rs. 12,00,000"
+  },
+  {  
+    "ID": 4,
+    "ROLE": "Backend Engineer",
+    "LOCATION": "San Francisco, U.S.A.",
+    "PAY": "$ 120,000"
+  }
+]
+
 @app.route("/")
-def hello_world():
-  return "Hello, World"
+def home_page():
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def jobs_page():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
